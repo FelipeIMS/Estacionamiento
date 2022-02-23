95f9a366b565234c904410e64f0ede7a38b61531
FelipeIMS <felipemendezsilv@gmail.com>
2022-02-23 13:31:54 -0300

    Update ingresar.php

--- user/simpleuser/ingresar.php
+++ user/simpleuser/ingresar.php
@@ -12,14 +12,14 @@
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     </script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="stylesheet" href="./css/estilos.css">
 </head>
 
 <body>
-    <div class="container_fluid col s12 m4 l8">
+    <div class="container_fluid">
         <nav>
             <div class="nav-wrapper #f3e5f5 purple lighten-5">
-                <a href="#" class="brand-logo"><img src="./img/logo-clinica-lircay.png" alt="Clinica Lircay" style="height:70px;"></a>
+                <a href="index.php" class="brand-logo"><img src="./img/logo-clinica-lircay.png" alt="Clinica Lircay"
+                        style="height:70px;"></a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
                     <li><a class="black-text" href="index.php">Volver</a></li>
                 </ul>
@@ -28,17 +28,18 @@
 
     </div>
 
-    <div class="container_fluid">
+    <div class="container">
         <div class="row">
-            <div class="col s6">
+            <form class="col s12">
                 <div class="row">
-                    <div class="input-field col s3">
+                    <div class="input-field col s2">
                         <input type="text" id="autocomplete-input" class="autocomplete">
                         <label for="autocomplete-input"><i class="fa-solid fa-message"></i> Autocomplete</label>
+                        <a class="waves-effect waves-light btn"><i class="fa-solid fa-floppy-disk"></i> Guardar</a>
                     </div>
                 </div>
-            </div>
-        </div>
+            </form>
+         </div>
     </div>
 
 
@@ -50,7 +51,8 @@
     });
     </script>
     <!-- JQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
