0f8fec9dd7094affb8721ced3fd00c59114c9c95
luissssssssssssssxd <98342890+luissssssssssssssxd@users.noreply.github.com>
2022-02-23 23:27:52 -0300

    insert_fichas ( falta los redirect y diseño) xd

--- user/simpleuser/footer.php
+++ user/simpleuser/footer.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<body>
-    
-</body>
-<script src="./js/jquery.js"></script>
 
-    <script src="./js/bootstrap.bundle.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js" integrity="sha256-xH4q8N0pEzrZMaRmd7gQVcTZiFei+HfRTBPJ1OGXC0k=" crossorigin="anonymous"></script>
     <script src="./js/bootstrap.js"></script>
     <script src="./js/datatable.js"></script>
     <script src="./js/sistema.js"></script>
-</html>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+
