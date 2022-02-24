048edd9452ea7110dde9ddef3b0427ca21385fc8
luissssssssssssssxd <98342890+luissssssssssssssxd@users.noreply.github.com>
2022-02-24 10:39:55 -0300

    update

--- includes/connect.php
+++ includes/connect.php
@@ -1,11 +1,11 @@
 <?php
-$dbhost = "localhost"; //Host
+/* $dbhost = "localhost"; //Host
 $dbuser = "root"; //Database user
-$dbpass = ""; //Database password
-// $dbname = "estacionamiento"; //Database name
-// $dbhost = "192.168.2.175:3306"; //Host
-// $dbuser = "username"; //Database user
-// $dbpass = "password"; //Database password
+$dbpass = ""; //Database password */
+ $dbname = "estacionamiento"; //Database name
+ $dbhost = "192.168.2.175:3306"; //Host
+$dbuser = "username"; //Database user
+$dbpass = "password"; //Database password
 $dbname = "estacionamiento"; //Database name
 $conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection
 mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
