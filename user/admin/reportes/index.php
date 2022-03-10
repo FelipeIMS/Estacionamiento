e2fd8f9367cd506439a26f50ee1fcaaccee3a3dc
luissssssssssssssxd <98342890+luissssssssssssssxd@users.noreply.github.com>
2022-03-10 19:18:19 -0300

    reporte grafico

--- user/admin/reportes/index.php
+++ user/admin/reportes/index.php
@@ -1,16 +1,6 @@
 <?php
-
 include '../settings.php';
-$conn->query("SET lc_time_names = 'es_ES'");
-$sql = "SELECT MONTHNAME(inicio) Mes, SUM(total) total_mes
-FROM ficha
-GROUP BY Mes
-ORDER BY Mes"; // Consulta SQL
-$query = $conn->query($sql); // Ejecutar la consulta SQL
-$data = array(); // Array donde vamos a guardar los datos
-while ($r = $query->fetch_object()) { // Recorrer los resultados de Ejecutar la consulta SQL
-    $data[] = $r; // Guardar los resultados en la variable $data
-}
+date_default_timezone_set("America/Santiago");
 
 ?>
 <!DOCTYPE html>
@@ -25,15 +15,47 @@ while ($r = $query->fetch_object()) { // Recorrer los resultados de Ejecutar la 
 </head>
 
 <body>
+
+    <?php
+
+
+    $date1 = isset($_POST['date1']) ? $_POST['date1'] : "";
+    $date2 = isset($_POST['date2']) ? $_POST['date2'] : "";
+
+
+    $conn->query("SET lc_time_names = 'es_ES'");
+    $sql = "SELECT DAY(inicio) Mes, SUM(total) total_mes
+    FROM ficha
+    WHERE inicio and termino between '$date1' and '$date2'
+    GROUP BY Mes
+     ORDER BY Mes";
+
+    $query = $conn->query($sql); // Ejecutar la consulta SQL
+    $data = array(); // Array donde vamos a guardar los datos
+    while ($r = $query->fetch_object()) { // Recorrer los resultados de Ejecutar la consulta SQL
+        $data[] = $r; // Guardar los resultados en la variable $data
+    }
+    ?>
+
+
+
     <div class="container">
         <h1 class="text-center">Ingresos Por Mes</h1>
-        <a class="btn btn-warning my-2" style="float:right" href="../index.php"><i class="fa-solid fa-arrow-left"></i></a>
+        <form method="post" class="text-center">
 
-        <canvas id="chart1" ></canvas>
+            <label>Desde: </label>
+            <input type="datetime-local"  id="date1"  name="date1" value=<?php echo date('Y-m-d\TH:i:s'); ?>  required>
+            <label>Hasta: </label>
+            <input type="datetime-local" id="date2"  name="date2"  value=<?php echo date('Y-m-d\TH:i:s'); ?>  required>
+            <button class="btn btn-success" type="submit">Generar</button>
+
+        </form>
+        <a class="btn btn-warning my-2" style="float:right" href="../index.php"><i class="fa-solid fa-arrow-left"></i></a>
+        <canvas id="chart1"></canvas>
 
     </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     <script>
         var ctx = document.getElementById("chart1");
         var data = {
@@ -42,15 +64,15 @@ while ($r = $query->fetch_object()) { // Recorrer los resultados de Ejecutar la 
                 <?php endforeach; ?>
             ],
             datasets: [{
-                label: '$ Ingresos por Mes',
+                label: 'Ingresos por Dia',
                 data: [
                     <?php foreach ($data as $d) : ?>
                         <?php echo $d->total_mes; ?>,
                     <?php endforeach; ?>
                 ],
-                backgroundColor: "#3898db",
-                borderColor: "#9b59b6",
-                borderWidth: 2
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgb(75, 192, 192)',
+                borderWidth: 1
             }]
         };
         var options = {
@@ -69,6 +91,12 @@ while ($r = $query->fetch_object()) { // Recorrer los resultados de Ejecutar la 
             options: options
         });
     </script>
+    <script>
+        document.getElementById("date1").addEventListener("input", () => console.log(document.getElementById("date1").value));
+        document.getElementById("date2").addEventListener("input", () => console.log(document.getElementById("date2").value));
+
+    </script>
+ 
 </body>
 
 </html>
