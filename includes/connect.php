d76b17a334d8b70289731b5c534bee8e8658d498
FelipeIMS <felipemendezsilv@gmail.com>
2022-03-11 10:48:02 -0300

    update-pagoxd

--- includes/connect.php
+++ includes/connect.php
@@ -1,10 +1,10 @@
 <?php
-//   $dbhost = "192.168.2.175:3306"; //Host
-// $dbuser = "username"; //Database user
-// $dbpass = "password"; //Database password
-$dbhost = "localhost"; //Host
-$dbuser = "root"; //Database user
-$dbpass = ""; //Database password */ 
+  $dbhost = "192.168.2.175:3306"; //Host
+$dbuser = "username"; //Database user
+$dbpass = "password"; //Database password
+// $dbhost = "localhost"; //Host
+// $dbuser = "root"; //Database user
+// $dbpass = ""; //Database password */ 
 $dbname = "estacionamiento"; //Database name
 $conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection
 mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
