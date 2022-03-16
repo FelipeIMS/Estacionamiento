041e535ef582cdecfb420406068376c91a2bf64b
luissssssssssssssxd <98342890+luissssssssssssssxd@users.noreply.github.com>
2022-03-16 19:33:35 -0300

    xd

--- user/admin/index/index.php
+++ user/admin/index/index.php
@@ -133,7 +133,7 @@ while ($r = $query3->fetch_object()) { // Recorrer los resultados de Ejecutar la
                 <div class="form-group mt-2">
                     <input disabled class="form-control w-50 text-center position-relative top-50 start-50 translate-middle" id="contador" type="text" name="contador" value="Espacios ocupados: <?php echo $espacios2[0]; ?> de 62" />
                     <div class="container" style="width: 200px;">
-                        <canvas id="chart3" ></canvas>
+                        <canvas id="chart3"></canvas>
 
                     </div>
 
@@ -371,53 +371,53 @@ while ($r = $query3->fetch_object()) { // Recorrer los resultados de Ejecutar la
             });
         </script>
 
-<script>
-        var ctx = document.getElementById("chart3");
-        var data = {
-            labels: ['Ocupados','Disponibles'],
-            datasets: [{
-                label: 'Espacios Estacionamiento:',
-                data: [
-                    <?php foreach ($data3 as $d) : ?>
-                        <?php echo $d->espacios; ?>,
-                        <?php echo $d->total_espacios; ?>,
-                    <?php endforeach; ?>
-                ],
-                backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-                borderWidth: 1
-            }]
-        };
-        var options = {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
+        <script>
+            var ctx = document.getElementById("chart3");
+            var data = {
+                labels: ['Ocupados', 'Disponibles'],
+                datasets: [{
+                    label: 'Espacios Estacionamiento:',
+                    data: [
+                        <?php foreach ($data3 as $d) : ?>
+                            <?php echo $d->espacios; ?>,
+                            <?php echo $d->total_espacios; ?>,
+                        <?php endforeach; ?>
+                    ],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
                 }]
-            }
-        };
-        var chart2 = new Chart(ctx, {
-            type: 'pie',
-            /* valores: line, bar*/
-            data: data,
-            options: options
-        });
-    </script>
+            };
+            var options = {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            };
+            var chart2 = new Chart(ctx, {
+                type: 'pie',
+                /* valores: line, bar*/
+                data: data,
+                options: options
+            });
+        </script>
 </body>
 
 </html>
--- user/admin/index/insert_ficha.php
+++ user/admin/index/insert_ficha.php
@@ -1,5 +1,5 @@
 <?php include '../settings.php'; //include settings 
-                require '../autoload.php';
+require '../autoload.php';
 
 use Mike42\Escpos\EscposImage;
 use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
@@ -21,92 +21,89 @@ use Mike42\Escpos\CapabilityProfile;
         $search = mysqli_real_escape_string($conn, $_POST["patente"]);
         $registro2 = mysqli_query($conn, "SELECT id_cliente, cliente.nombre_cliente, cliente.apellido_cliente from cliente
         inner join vehiculo on vehiculo.cliente = cliente.id_cliente
-        where cliente.estado = 'Inactivo' and vehiculo.patente = '$search';");  
+        where cliente.estado = 'Inactivo' and vehiculo.patente = '$search';");
         if (mysqli_num_rows($registro2) > 0) {
             echo '<script>toastr.error("Error al ingresar, finalize la boleta anterior")</script>';
             header("refresh: 1; url=index.php");
-            } else {
-                $espacios = "UPDATE espacios set espacios = espacios - 1  where id = 1;";
-                $resultadoEspacios = mysqli_query($conn, $espacios);
+        } else {
+            $espacios = "UPDATE espacios set espacios = espacios - 1  where id = 1;";
+            $resultadoEspacios = mysqli_query($conn, $espacios);
 
-                $sql = " INSERT INTO ficha(inicio,patente,espacio_ocupado,user_ficha, estado)  VALUES(now(),'$search',1,'{$_SESSION['id']}','No pagado')";
-                $sql2="UPDATE cliente c
+            $sql = " INSERT INTO ficha(inicio,patente,espacio_ocupado,user_ficha, estado)  VALUES(now(),'$search',1,'{$_SESSION['id']}','No pagado')";
+            $sql2 = "UPDATE cliente c
                 JOIN vehiculo v  ON c.id_cliente = v.cliente
                 SET c.estado= 'Inactivo'
                 WHERE v.patente='$search';";
-                mysqli_query($conn,$sql2);
-                if (mysqli_query($conn, $sql)) {
-                    echo '<script>toastr.success("Ficha ingresada correctamente")</script>';
-            header("refresh: 1; url=index.php");
+            mysqli_query($conn, $sql2);
+            if (mysqli_query($conn, $sql)) {
+                echo '<script>toastr.success("Ficha ingresada correctamente")</script>';
+                header("refresh: 1; url=index.php");
 
 
-                    $selectUltimo = "SELECT * from ficha order by id_ficha desc limit 1;";
+                $selectUltimo = "SELECT * from ficha order by id_ficha desc limit 1;";
 
-                    $resultado = mysqli_query($conn, $selectUltimo);
+                $resultado = mysqli_query($conn, $selectUltimo);
 
-                    $row = mysqli_fetch_array($resultado);
+                $row = mysqli_fetch_array($resultado);
+            }
 
-                }
+            $nombre_host = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+            $profile = CapabilityProfile::load("simple");
+            $connector = new WindowsPrintConnector("smb://$nombre_host/boletas");
+            $printer = new Printer($connector, $profile);
 
-                $nombre_host = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-                $profile = CapabilityProfile::load("simple");
-                $connector = new WindowsPrintConnector("smb://$nombre_host/boletas");
-$printer = new Printer($connector, $profile);
-
-/*
+            /*
      Imprimimos un mensaje. Podemos usar
      el salto de línea o llamar muchas
      veces a $printer->text()
  */
-$printer->setJustification(Printer::JUSTIFY_CENTER);
-$printer->text("Inmobiliaria Lircay" . "\n");
-$printer->text("2 Poniente 1380, Talca" . "\n");
-$printer->setJustification(Printer::JUSTIFY_LEFT);
-$printer->text( "\n");
-$printer->text("Ticket  Entrada" . "\n");
-$printer->text( "\n");
-$printer->text("Boleta N°: " . $row['id_ficha'] . "\n");
-$printer->text( "\n");
-$printer->text("Inicio: " . $row['inicio']  . "\n");
-$printer->text( "\n");
-$printer->text("Operador: " . $_SESSION['name']  . "\n");
+            $printer->setJustification(Printer::JUSTIFY_CENTER);
+            $printer->text("Inmobiliaria Lircay" . "\n");
+            $printer->text("2 Poniente 1380, Talca" . "\n");
+            $printer->setJustification(Printer::JUSTIFY_LEFT);
+            $printer->text("\n");
+            $printer->text("Ticket  Entrada" . "\n");
+            $printer->text("\n");
+            $printer->text("Boleta N°: " . $row['id_ficha'] . "\n");
+            $printer->text("\n");
+            $printer->text("Inicio: " . $row['inicio']  . "\n");
+            $printer->text("\n");
+            $printer->text("Operador: " . $_SESSION['name']  . "\n");
 
 
 
 
-/*
+            /*
      Hacemos que el papel salga. Es como 
      dejar muchos saltos de línea sin escribir nada
  */
-$printer->feed(8);
+            $printer->feed(8);
 
 
 
-/*
+            /*
      Cortamos el papel. Si nuestra impresora
      no tiene soporte para ello, no generará
      ningún error
  */
-$printer->cut();
+            $printer->cut();
 
-/*
+            /*
      Por medio de la impresora mandamos un pulso.
      Esto es útil cuando la tenemos conectada
      por ejemplo a un cajón
  */
-$printer->pulse();
+            $printer->pulse();
 
-/*
+            /*
      Para imprimir realmente, tenemos que "cerrar"
      la conexión con la impresora. Recuerda incluir esto al final de todos los archivos
  */
-$printer->close();
-            
-            }
-        
-    }else{
+            $printer->close();
+        }
+    } else {
         echo '<script>toastr.error("Error al ingresar nueva ficha")</script>';
-            header("refresh: 1; url=index.php");
+        header("refresh: 1; url=index.php");
     }
 
 
