cbed6d7a0ba240540004f258a1260d7c5d6bb3bd
luissssssssssssssxd <98342890+luissssssssssssssxd@users.noreply.github.com>
2022-03-23 19:44:59 -0300

    highcharts

--- user/admin/reportes/index.php
+++ user/admin/reportes/index.php
@@ -2,6 +2,68 @@
 include '../settings.php';
 date_default_timezone_set("America/Santiago");
 
+
+/* Entradas primeros 7 dias */
+$sql = "SELECT DAY(inicio) DIA,COUNT(inicio) REGISTROS FROM ficha
+WHERE WEEK(inicio)= WEEK(CURDATE()) AND
+inicio BETWEEN date_add(NOW(), INTERVAL -6 DAY) AND NOW()
+GROUP BY DIA,DAYNAME(inicio)
+ORDER BY inicio";
+$sql2 = "SELECT DAY(inicio) DIA,COUNT(inicio) REGISTROS FROM ficha
+WHERE WEEK(inicio)= WEEK(CURDATE()) AND
+inicio BETWEEN date_add(NOW(), INTERVAL -6 DAY) AND NOW()
+GROUP BY DIA,DAYNAME(inicio)
+ORDER BY inicio";
+$viewer = mysqli_query($conn, $sql);
+$viewer2 = mysqli_query($conn, $sql2);
+$viewer = mysqli_fetch_all($viewer, MYSQLI_ASSOC);
+$viewer2 = mysqli_fetch_all($viewer2, MYSQLI_ASSOC);
+$viewer = json_encode(array_column($viewer, 'DIA'), JSON_NUMERIC_CHECK);
+$viewer2 = json_encode(array_column($viewer2, 'REGISTROS'), JSON_NUMERIC_CHECK);
+
+
+/* Entradas ultimos 7 dias */
+$sql = "SELECT DAY(termino) DIA,COUNT(termino) REGISTROS FROM ficha
+WHERE WEEK(termino)= WEEK(CURDATE()) AND
+termino BETWEEN date_add(NOW(), INTERVAL -6 DAY) AND NOW()
+GROUP BY DIA,DAYNAME(termino)
+ORDER BY termino";
+$sql2 = "SELECT DAY(termino) DIA,COUNT(termino) REGISTROS FROM ficha
+WHERE WEEK(termino)= WEEK(CURDATE()) AND
+termino BETWEEN date_add(NOW(), INTERVAL -6 DAY) AND NOW()
+GROUP BY DIA,DAYNAME(termino)
+ORDER BY termino";
+$terminoid = mysqli_query($conn, $sql);
+$terminoval = mysqli_query($conn, $sql2);
+$terminoid = mysqli_fetch_all($terminoid, MYSQLI_ASSOC);
+$terminoval = mysqli_fetch_all($terminoval, MYSQLI_ASSOC);
+$terminoid = json_encode(array_column($terminoid, 'DIA'), JSON_NUMERIC_CHECK);
+$terminoval = json_encode(array_column($terminoval, 'REGISTROS'), JSON_NUMERIC_CHECK);
+
+
+/* Registro por dia*/
+
+$datenew = isset($_POST['datenew']) ? $_POST['datenew'] : "";
+$datenew2 = isset($_POST['datenew2']) ? $_POST['datenew2'] : "";
+$conn->query("SET lc_time_names = 'es_ES'");
+$sql = "SELECT DAY(fecha_pago) Mes, SUM(total) total_mes
+FROM ficha
+WHERE fecha_pago and fecha_pago between '$datenew' and '$datenew2'
+GROUP BY Mes
+ORDER BY Mes";
+$sql2 = "SELECT DAY(fecha_pago) Mes, SUM(total) total_mes
+FROM ficha
+WHERE fecha_pago and fecha_pago between '$datenew' and '$datenew2'
+GROUP BY Mes
+ORDER BY Mes";
+
+$mesid = mysqli_query($conn, $sql);
+$mesval = mysqli_query($conn, $sql2);
+$mesid = mysqli_fetch_all($mesid, MYSQLI_ASSOC);
+$mesval = mysqli_fetch_all($mesval, MYSQLI_ASSOC);
+$mesid = json_encode(array_column($mesid, 'Mes'), JSON_NUMERIC_CHECK);
+$mesval = json_encode(array_column($mesval, 'total_mes'), JSON_NUMERIC_CHECK);
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,8 +76,103 @@ date_default_timezone_set("America/Santiago");
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
 </head>
-</head>
+
+<script>
+    $(function() {
+
+
+        var data_viewer = <?php echo $viewer; ?>;
+        var data2 = <?php echo $viewer2; ?>;
+
+
+        $('#container').highcharts({
+            chart: {
+                type: 'line'
+            },
+            title: {
+                text: 'Entradas de los ultimos 7 dias'
+            },
+            xAxis: {
+
+                categories: data_viewer
+            },
+            yAxis: {
+                title: {
+                    text: 'Cantidad'
+                }
+            },
+            series: [{
+                name: 'Cantidad',
+                data: data2
+            }]
+        });
+    });
+</script>
+<script>
+    $(function() {
+
+
+        var terminoid = <?php echo $terminoid; ?>;
+        var terminoval = <?php echo $terminoval; ?>;
+
+
+        $('#salidas').highcharts({
+            chart: {
+                type: 'line'
+            },
+            title: {
+                text: 'Salidas de los ultimos 7 dias'
+            },
+            xAxis: {
+
+                categories: terminoid
+            },
+            yAxis: {
+                title: {
+                    text: 'Cantidad'
+                }
+            },
+            series: [{
+                name: 'Cantidad',
+                data: terminoval
+            }]
+        });
+    });
+</script>
+
+<script>
+    $(function() {
+
+
+        var mesid = <?php echo $mesid; ?>;
+        var mesval = <?php echo $mesval; ?>;
+
+
+        $('#pordia').highcharts({
+            chart: {
+                type: 'line'
+            },
+            title: {
+                text: 'Ingresos por dia'
+            },
+            xAxis: {
+
+                categories: mesid
+            },
+            yAxis: {
+                title: {
+                    text: 'Cantidad'
+                }
+            },
+            series: [{
+                name: 'Cantidad',
+                data: mesval
+            }]
+        });
+    });
+</script>
 
 <body>
 
@@ -49,7 +206,7 @@ date_default_timezone_set("America/Santiago");
     GROUP BY DIA,DAYNAME(fecha_pago)
     ORDER BY fecha_pago";
 
- /*    $sql2 = "SELECT DAY(inicio) Mes, SUM(total) total_dia,users.name as user_venta
+    /*    $sql2 = "SELECT DAY(inicio) Mes, SUM(total) total_dia,users.name as user_venta
     FROM ficha
     INNER JOIN users ON users.id = ficha.user_ficha_out
     WHERE ficha.inicio and ficha.termino between '$datenew' and '$datenew2' AND
@@ -65,29 +222,6 @@ date_default_timezone_set("America/Santiago");
 
     }
 
-    $sql3 = "SELECT DAY(inicio) DIA,COUNT(inicio) REGISTROS FROM ficha
-    WHERE WEEK(inicio)= WEEK(CURDATE()) AND
-    inicio BETWEEN date_add(NOW(), INTERVAL -6 DAY) AND NOW()
-    GROUP BY DIA,DAYNAME(inicio)
-    ORDER BY inicio";
-    $query3 = $conn->query($sql3); // Ejecutar la consulta SQL
-    $data3 = array(); // Array donde vamos a guardar los datos
-    while ($r = $query3->fetch_object()) { // Recorrer los resultados de Ejecutar la consulta SQL
-        $data3[] = $r; // Guardar los resultados en la variable $data
-
-    }
-
-    $sql4 = "SELECT DAY(termino) DIA,COUNT(termino) REGISTROS FROM ficha
-    WHERE WEEK(termino)= WEEK(CURDATE()) AND
-    termino BETWEEN date_add(NOW(), INTERVAL -6 DAY) AND NOW()
-    GROUP BY DIA,DAYNAME(termino)
-    ORDER BY termino";
-    $query4 = $conn->query($sql4);
-    $data4 = array(); // Array donde vamos a guardar los datos
-    while ($r = $query4->fetch_object()) { // Recorrer los resultados de Ejecutar la consulta SQL
-        $data4[] = $r; // Guardar los resultados en la variable $data
-
-    }
 
     ?>
 
@@ -97,22 +231,79 @@ date_default_timezone_set("America/Santiago");
             margin-top: 1.9em;
         }
     </style>
-
     <div class="container">
-        <h1 class="text-center">Ingresos Por Mes</h1>
-        <form method="post" class="text-center">
-
-            <label>Desde: </label>
-            <input type="datetime-local" id="date1" name="date1" value=<?php echo date('Y-m-d\TH:i:s'); ?> required>
-            <label>Hasta: </label>
-            <input type="datetime-local" id="date2" name="date2" value=<?php echo date('Y-m-d\TH:i:s'); ?> required>
-            <button class="btn btn-success" type="submit">Generar</button>
-
-        </form>
-        <a class="btn btn-warning my-2" style="float:right" href="../index/index.php"><i class="fa-solid fa-arrow-left"></i></a>
-        <canvas id="chart1"></canvas>
-
+        <br />
+        <h2 class="text-center">Entradas</h2>
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
+                    <div class="panel-body">
+                        <div id="container"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    <div class="container">
+        <br />
+        <h2 class="text-center">Salidas</h2>
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
+                    <div class="panel-body">
+                        <div id="salidas"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <br />
+        <h2 class="text-center">Ingresos por dias</h2>
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
+                    <div class="panel-body" style="margin-left: 400px; width: 1000px;" >
+                        <form method="post">
+                            <div class="row" >
+                                <div class="col-sm-3">
+                                    <div class="form-group">
+                                        <label>Desde: </label>
+                                        <input class="form-control" type="datetime-local" id="datenew" name="datenew" value=<?php echo date('Y-m-d\TH:i:s'); ?> required>
+
+                                    </div>
+
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="form-group">
+                                        <label>Hasta: </label>
+                                        <input class="form-control" type="datetime-local" id="datenew2" name="datenew2" value=<?php echo date('Y-m-d\TH:i:s'); ?> required>
+                                    </div>
+
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="d-flex justify-content-around my-4">
+                                        <button class="btn btn-sm btn-outline-secondary btn-bottom-left" type="submit">
+                                            <i class="fas fa-search"></i> Buscar
+                                        </button>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                    <div id="pordia"></div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+ 
     <div class="container">
         <h1 class="text-center">Movimientos por usuario</h1>
         <form method="post" class="text-center">
@@ -162,26 +353,12 @@ date_default_timezone_set("America/Santiago");
         <canvas id="chart2"></canvas>
 
     </div>
-    <div class="container text-center">
-        <h1 class="text-center">Ingresos y Salidas</h1>
-        <label>Desde: </label>
-        <input type="datetime-local" id="date1" name="date1" value=<?php echo date('Y-m-d\TH:i:s'); ?> required>
-        <label>Hasta: </label>
-        <input type="datetime-local" id="date2" name="date2" value=<?php echo date('Y-m-d\TH:i:s'); ?> required>
-        <button class="btn btn-success" type="submit">Generar</button>
-    </div>
 
-    <div class="container">
-        <h1 class="text-center">Ingresos</h1>
-        <canvas id="chart3"></canvas>
-    </div>
-    <div class="container">
-        <h1 class="text-center">Salidas</h1>
-        <canvas id="chart4"></canvas>
-    </div>
+
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    <script>
+  <!--   <script>
         var ctx = document.getElementById("chart1");
         var data = {
             labels: [
@@ -209,17 +386,17 @@ date_default_timezone_set("America/Santiago");
                 }]
             }
         };
-        var chart3 = new Chart(ctx, {
+        var chart1 = new Chart(ctx, {
             type: 'line',
             /* valores: line, bar*/
             data: data,
             options: options
         });
-    </script>
-    <script>
+    </script> -->
+   <!--  <script>
         document.getElementById("date1").addEventListener("input", () => console.log(document.getElementById("date1").value));
         document.getElementById("date2").addEventListener("input", () => console.log(document.getElementById("date2").value));
-    </script>
+    </script> -->
     <script>
         $('.postName').select2({
             placeholder: 'Select an item',
@@ -241,111 +418,9 @@ date_default_timezone_set("America/Santiago");
             }
         });
     </script>
-    <script>
-        var ctx = document.getElementById("chart3");
-        var data = {
-            labels: [
-                <?php foreach ($data3 as $d) : ?> "<?php echo $d->DIA ?>",
-                <?php endforeach; ?>
-            ],
-            datasets: [{
-                label: 'Ingresos ultimos 7 dias:',
-                data: [
-                    <?php foreach ($data3 as $d) : ?>
-                        <?php echo $d->REGISTROS; ?>,
-                    <?php endforeach; ?>
-                ],
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgb(75, 192, 192)',
-                borderWidth: 1
-            }]
-        };
-        var options = {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        };
-        var chart3 = new Chart(ctx, {
-            type: 'line',
-            /* valores: line, bar*/
-            data: data,
-            options: options
-        });
-    </script>
-    <script>
-        var ctx = document.getElementById("chart2");
-        var data = {
-            labels: [
-                <?php foreach ($data2 as $d) : ?> "<?php echo $d->DIA ?>",
-                <?php endforeach; ?>
-            ],
-            datasets: [{
-                label: ' <?php echo $d->user_venta; ?>',
-                data: [
-                    <?php foreach ($data2 as $d) : ?>
-                        <?php echo $d->total_dia; ?>,
-                    <?php endforeach; ?>
-                ],
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgb(75, 192, 192)',
-                borderWidth: 1
-            }]
-        };
-        var options = {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        };
-        var chart2 = new Chart(ctx, {
-            type: 'bar',
-            /* valores: line, bar*/
-            data: data,
-            options: options
-        });
-    </script>
-    <script>
-        var ctx = document.getElementById("chart4");
-        var data = {
-            labels: [
-                <?php foreach ($data4 as $d) : ?> "<?php echo $d->DIA ?>",
-                <?php endforeach; ?>
-            ],
-            datasets: [{
-                label: 'Salidas en los ultimos 7 dias',
-                data: [
-                    <?php foreach ($data4 as $d) : ?>
-                        <?php echo $d->REGISTROS; ?>,
-                    <?php endforeach; ?>
-                ],
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgb(75, 192, 192)',
-                borderWidth: 1
-            }]
-        };
-        var options = {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        };
-        var chart4 = new Chart(ctx, {
-            type: 'line',
-            /* valores: line, bar*/
-            data: data,
-            options: options
-        });
-    </script>
+
+
+
 </body>
 
 </html>
